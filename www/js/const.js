9bc488bddcc637ad09611ac6bd272e24b3981ad1
ofirrifo <ofirrifo@gmail.com>
2016-03-26 10:20:06 +0300

    first commit

---
+++ www/js/const.js
@@ -0,0 +1,13 @@
+angular.module('starter')
+  .constant('appConst', {
+    dictionary: {
+      menu: "תפריט",
+      activities: "פעולות",
+      loadCard: "טעינת כרטיס",
+      sendCard: "שליחת כרטיס",
+      billingInformation: "פרטי חיוב",
+      settings: "הגדרות",
+      logOut: "יציאה",
+      nis: 'ש"ח'
+    }
+  });
